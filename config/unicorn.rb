3d9e11ed145850bfcdc47c4fda330ac022942a05
William LaFrance <admin@wjlafrance.net>
2012-02-15 14:16:35 -0600

    potential fix for postgres concurrency issues

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,3 +1,7 @@
 worker_processes 4 # amount of unicorn workers to spin up
 timeout 30         # restarts workers that hang for 30 seconds
 preload_app true
+
+after_fork do |server, worker| 
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection 
+end
