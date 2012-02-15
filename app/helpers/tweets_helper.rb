ea9b6ff003f66f88e1434ad55c556a6cc88a207d
William LaFrance <admin@wjlafrance.net>
2012-02-15 14:08:36 -0600

    Handling display of native retweets

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -1,9 +1,5 @@
 module TweetsHelper
 
-  def profile_image_for(tweet)
-    image_tag tweet.user.profile_image_url, :alt => "Profile image", :size => "100x100"
-  end
-
   def navigation_bar
     return "<span class=\"pages\">#{link_to "Log In", login_path}</span>" unless @user
 
@@ -32,9 +28,43 @@ module TweetsHelper
     '<tr>' +
     "<td class=\"tweet_image\">#{profile_image_for tweet}" +
     "<td class=\"tweet_text\">" +
-      "<b>@#{tweet.user.screen_name}</b> (#{tweet.user.name})<br>" +
-      "#{tweet.text}<br>" +
-      "<span class=\"timestamp\">#{time_ago_in_words(tweet.created_at)} ago</span>"
+      "#{name_for tweet}<br>" +
+      "#{text_for tweet}<br>" +
+      "#{timestamp_for tweet}"
   end
 
+  def name_for(tweet)
+    unless tweet.retweeted_status
+      "<b>@#{tweet.user.screen_name}</b> (#{tweet.user.name})"
+    else
+      "<b>@#{tweet.retweeted_status.user.screen_name}</b> (#{tweet.retweeted_status.user.name})"
+    end
+  end
+  
+  def timestamp_for(tweet)
+    unless tweet.retweeted_status
+      "<span class=\"timestamp\">#{time_ago_in_words(tweet.created_at)} ago</span>"
+    else
+      "<span class=\"timestamp\">#{time_ago_in_words(tweet.created_at)} ago</span><br>" +
+      "<span class=\"timestamp\">retweeted #{time_ago_in_words(tweet.retweeted_status.created_at)} " +
+      "ago by <b>@#{tweet.user.screen_name}</b> (#{tweet.user.name})</span>"
+    end
+  end
+  
+  def profile_image_for(tweet)
+    unless tweet.retweeted_status
+      image_tag tweet.user.profile_image_url, :alt => "Profile image", :size => "100x100"
+    else
+      image_tag tweet.retweeted_status.user.profile_image_url, :alt => "Profile image", :size => "100x100"
+    end
+  end
+  
+  def text_for(tweet)
+    unless tweet.retweeted_status
+      tweet.text
+    else
+      tweet.retweeted_status.text
+    end
+  end
+  
 end
